fa8be78c5d706372f3d940940a12ea7dc36feb4f
FontellaH <136127924+FontellaH@users.noreply.github.com>
2023-06-21 07:15:01 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//<----- Alerts when the likes btn is clicked----->
+
+function logOn(element) {
+    element.innerText = "Log Out";
+}
+
+// <------ Hide the btn ----->
+
+function hide(element) {
+    element.remove();
+}
